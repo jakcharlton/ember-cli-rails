e9b0679f9c21897b3f53d5ea16f57b8174767da2
Jak Charlton <jakcharlton@gmail.com>
2014-12-22 16:07:16 +1100

    Rout to index

--- babylon-api/config/routes.rb
+++ babylon-api/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, controllers: { sessions: 'sessions' }
 
+
+  root 'application#index'
+
   ActiveAdmin.routes(self)
 
   mount API::Base, at: "/api"
