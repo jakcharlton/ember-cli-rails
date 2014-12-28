b29d2d4fd745d451b7ddcbfce071beb98b8f0de0
Jak Charlton <jakcharlton@gmail.com>
2014-12-28 11:37:29 +1100

    Fixing untested change

--- babylon-api/config/initializers/redis.rb
+++ babylon-api/config/initializers/redis.rb
@@ -1,5 +1,3 @@
-# Configuration settings
-module Settings
-  uri = Rails.env.development? ? URI.parse('redis://localhost:6379/') : URI.parse(ENV['REDISCLOUD_URL'])
-  REDIS = Redis.new host: uri.host, port: uri.port, password: uri.password
-end
+uri = Rails.env.development? ? URI.parse('redis://localhost:6379/') : URI.parse(ENV['REDISCLOUD_URL'])
+
+BabylonApi::Application.config.redis_uri = uri
